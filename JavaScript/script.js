f04561ea462b17517fd6b9cbc5cc38bd1d0e3f2b
Technix19 <109783947+Technix19@users.noreply.github.com>
2024-10-20 19:07:05 +0800

    Add files via upload

---
+++ JavaScript/script.js
@@ -0,0 +1,26 @@
+const hamburger = document.querySelector(".hamburger");
+const navMenu = document.querySelector(".nav-menu");
+
+hamburger.addEventListener("click", () => {
+    hamburger.classList.toggle("active");
+    navMenu.classList.toggle("active");
+})
+
+
+
+/* FAQ Section */
+document.addEventListener("DOMContentLoaded", () => {
+    const questions = document.querySelectorAll(".faq-question");
+
+    questions.forEach((question) => {
+        question.addEventListener("click", () => {
+            // Toggle the display of the answer
+            const answer = question.nextElementSibling;
+            if (answer.style.display === "block") {
+                answer.style.display = "none";
+            } else {
+                answer.style.display = "block";
+            }
+        });
+    });
+});
